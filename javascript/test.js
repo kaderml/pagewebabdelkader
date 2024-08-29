11b49fb58b18afded1b157b8275aa833c5a2b234
kaderml <152106661+kaderml@users.noreply.github.com>
2024-08-30 00:22:25 +0200

    Update test.js

--- javascript/test.js
+++ javascript/test.js
@@ -1,14 +1,3 @@
-// script.js
-document.addEventListener('DOMContentLoaded', (event) => {
-    let headerTitle = document.querySelector('h1');
-
-    function animateTitle() {
-        headerTitle.style.transform = 'translateX(0)'; // Amène le titre au centre
-    }
-
-    // Initialement, placez le titre hors de l'écran à droite
-    headerTitle.style.transform = 'translateX(500vw)';
-
-    // Démarre l'animation après un court délai pour garantir que le style initial est appliqué
-    setTimeout(animateTitle, 200);
-});
+const a = 2*4
+const b = 2+3
+console.log(a);
